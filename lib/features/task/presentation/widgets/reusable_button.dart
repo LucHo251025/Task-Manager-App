0b45dcf0aea8f296de1ee41ea584e06454b4db87
Luc <lucht.23itb@vku.udn.vn>
2025-06-16 23:56:40 +0700

    add task_group_card

---
+++ lib/features/task/presentation/widgets/reusable_button.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class ReusableButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+  final IconData? icon;
+
+  const ReusableButton({
+    Key? key,
+    required this.text,
+    required this.onPressed,
+    this.icon,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        onPressed: onPressed,
+        child: Row(
+          children: [
+            
+          ],
+        )
+    )
+  }
+}
--- lib/features/task/presentation/widgets/task_group_card.dart
+++ lib/features/task/presentation/widgets/task_group_card.dart
@@ -4,21 +4,95 @@ import 'package:task_manager_app/features/task/domain/entities/task_group_entity
 
 class TaskGroupCard extends StatelessWidget {
   final TaskGroupEntity group;
+
   TaskGroupCard({required this.group});
 
   @override
   Widget build(BuildContext context) {
     return Card(
-        margin: const EdgeInsets.all(8.0),
-        elevation: 2.0,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12.0),
-        ),
+      color: Colors.white,
+      margin: EdgeInsets.all(12),
+      elevation: 3.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12.0),
+      ),
+      child: Padding(
+        padding: EdgeInsets.all(16),
         child: Row(
           children: [
-            
+            Container(
+              width: 50,
+              height: 50,
+              decoration: BoxDecoration(
+                color: Color(0xFFFDE6F0),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Icon(
+                Icons.work_outline,
+                color: Color(0xFFED77B4),
+                size: 28,
+              ),
+            ),
+
+
+            SizedBox(width: 16),
+
+            Expanded(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    group.name,
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 4),
+                  Text(
+                    '${group.completedTasks} / ${group.totalTasks} tasks completed',
+                    style: TextStyle(color: Colors.grey[600]),
+                  ),
+                ],
+              ),
+            ),
+
+            SizedBox(width: 16),
+
+            buildPercentageCircle(group.completedTasks.toDouble(), group.totalTasks.toDouble()),
+
           ],
         ),
+      ),
     );
   }
 }
+
+Widget buildPercentageCircle(double completed, double total) {
+  double percentage = total > 0 ? (completed / total) : 0;
+
+  return Stack(
+    alignment: Alignment.center,
+    children: [
+      SizedBox(
+        width: 50,
+        height: 50,
+        child: CircularProgressIndicator(
+          value: percentage,
+          strokeWidth: 5,
+          backgroundColor: Color(0xFFFDE6F0), // Màu nền của vòng tròn
+          valueColor: AlwaysStoppedAnimation<Color>(Color(0xFFED77B4)),
+        ),
+      ),
+      Text(
+        '${(percentage * 100).toStringAsFixed(0)}%',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 12,
+        ),
+      ),
+    ],
+  );
+}
+
